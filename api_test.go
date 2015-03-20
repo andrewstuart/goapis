8ce7e2bb6cc6899e92097a3e4009f5f450b8b4d7
Andrew Stuart <andrew.stuart2@gmail.com>
2015-03-20 00:46:14 -0700

    Add initial test for API

---
+++ api_test.go
@@ -0,0 +1,28 @@
+package apis
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGet(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		q := r.URL.Query()
+
+		if q["test"][0] != "test" {
+			t.Errorf("Test header was not present")
+		}
+
+		fmt.Fprintln(w, "")
+	}))
+	defer ts.Close()
+
+	c := NewClient(ts.URL)
+
+	c.DefaultQuery(Query{
+		"test": "test",
+	})
+}
